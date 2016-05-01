c62bc9de019d24000bea0334c0fdca63c3e5ded3
hhyeonseok <hyunsuk@happycart.org>
2016-05-01 13:35:32 +0000

    라우트 지정하기

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Mvc::Application.routes.draw do
+	get 'gome/hello' => 'gome#hello'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
