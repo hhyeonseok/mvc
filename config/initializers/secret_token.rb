757fd65442a2c5512df97bac0dd52655cea75a73
hhyeonseok <hyunsuk@happycart.org>
2016-05-01 13:13:25 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mvc::Application.config.secret_key_base = 'd0bed239e9e5a1519857a6f1cd6a13d79ec081689490d6d5494ec6fe866ba9c440eef6a66972c100020470e550a3a845f11456630055977b897739957ef00751'
